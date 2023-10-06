31e45982b20245dd5c313e303bb8e34955e3898b
nikagelo <nikagelo@gmail.com>
2023-10-07 00:13:20 +0400

    finished task

---
+++ Taks01/Task01/Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿Random random = new Random();
+int randNum = random.Next(1, 11);
+int tries = 3;
+Console.WriteLine("Try to guess a number from 1 to 10");
+for (int i = 0; i < tries; i++)
+{
+    int guess;
+    int.TryParse(Console.ReadLine(), out guess);
+
+    if (guess == randNum)
+    {
+        Console.WriteLine("You won");
+        break;
+    }
+    else if (i == 2)
+    {
+        Console.WriteLine("Wrong. It was " + randNum);
+    }
+    else
+    {
+        Console.WriteLine("Wrong, try again");
+    }
+}
