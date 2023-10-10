244e52701bfe342bc28b3153ee378ea5dca79a1a
nikagelo <nikagelo@gmail.com>
2023-10-10 19:56:24 +0400

    finished task03

--- Task03/Task03/Task03/Program.cs
+++ Task03/Task03/Task03/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿///////////////////////////////////////////////პირველი დავალება
+//Console.WriteLine("Enter the size of an array");
+//string count = Console.ReadLine();
+//int countInt;
+//int.TryParse(count, out countInt);
+//int[] arr = new int[countInt];
+//for (int i = 0; i < countInt; i++)
+//{
+//    Console.WriteLine("Enter the element of an array");
+//    string input = Console.ReadLine();
+//    int inputInt;
+//    inputInt = int.Parse(input);
+//    arr[i] = inputInt;
+//}
+
+
+//int CalculateSumOfIndex(params int[] arr)
+//{
+//    Console.WriteLine("Enter the index");
+//    string index = Console.ReadLine();
+//    int indexInt;
+//    int.TryParse(index, out indexInt);
+//    int number = arr[indexInt];
+//    int sum = 0;
+
+//    while (number != 0)
+//    {
+//        int digit = number % 10;
+//        sum += digit;
+//        number /= 10;
+//    }
+//    Console.WriteLine(sum);
+//    return sum;
+//}
+//CalculateSumOfIndex(arr);
+
+//////////////////////////////////////////მეორე დავალება
+Console.WriteLine("Enter the size of an array");
+string count = Console.ReadLine();
+int countInt;
+int.TryParse(count, out countInt);
+char[] arr = new char[countInt];
+for (int i = 0; i < countInt; i++)
+{
+    Console.WriteLine("Enter the element of an array");
+    string input = Console.ReadLine();
+    arr[i]= char.Parse(input);
+  
+}
+Console.WriteLine("Enter the char to find");
+string toFind = Console.ReadLine();
+char toFindChar = char.Parse(toFind);
+int ReturnChars() {
+    int count = 0;
+    foreach (char x in arr)
+    {
+        if (toFindChar == x)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("There is "+count+" "+toFindChar);
+    return count;
+}
+ReturnChars();
