c017bce57fac997cf9e5b525d3954967fefbe9d0
marikuna <marykharaishvili28@gmail.com>
2023-10-20 22:01:11 +0400

    fixed static problem

--- Day04/Calculator/Calculator/Program.cs
+++ Day04/Calculator/Calculator/Program.cs
@@ -3,11 +3,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Calculator calc = new Calculator();
-        //calc.Add(5, 6);
-        //calc.Subtract(5, 6);
-        //calc.Multiply(5, 6);
-        //calc.Divide(5, 6);
         int sum = (int)Calculator.Add(5, 6);
         int substract = (int)Calculator.Subtract(5, 6);
         int multiply = (int)Calculator.Multiply(5, 6);
