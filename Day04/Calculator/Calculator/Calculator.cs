205bd6590520e3642801ec2fc1a323fcf06a61f5
marikuna <marykharaishvili28@gmail.com>
2023-10-20 21:58:51 +0400

    Finished Day04 Second Task

--- Day04/Calculator/Calculator/Calculator.cs
+++ Day04/Calculator/Calculator/Calculator.cs
@@ -1,26 +1,26 @@
 ﻿namespace CalculatorClass;
-public class Calculator
+public static class Calculator
 {
-    public float Add(float a, float b) { 
+    public static float Add(float a, float b) { 
         float sum = a+ b;
         Console.WriteLine("a + b = "+sum);
         return sum;
 
     }
-    public float Subtract(float a, float b)
+    public static float Subtract(float a, float b)
     {
         float minus = a - b;
         Console.WriteLine("a - b = " + minus);
         return minus;
 
     }
-    public float Multiply(float a, float b)
+    public static float Multiply(float a, float b)
     {
         float product = a * b;
         Console.WriteLine("a * b = " + product);
         return product;
     }
-    public float Divide(float a, float b)
+    public static float Divide(float a, float b)
     {
         float left = a / b;
         Console.WriteLine("a / b = " + left);
