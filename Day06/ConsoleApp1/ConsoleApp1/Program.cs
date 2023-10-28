eacfbdb10587da79a32e46c1e14d8776488f1cbf
nikagelo <nikagelo@gmail.com>
2023-10-28 23:46:58 +0400

    finished Day06 task

---
+++ Day06/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp1;
+Console.WriteLine("Enter the first side of a first triangle: ");
+double left1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second side of a first triangle: ");
+double left2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third side of a first triangle: ");
+double left3 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the first side of a second triangle: ");
+double right1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second side of a second triangle: ");
+double right2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the third side of a second triangle: ");
+double right3 = double.Parse(Console.ReadLine());
+
+
+Triangle leftTriangle = new Triangle(left1, left2, left3);
+Triangle rightTriangle = new Triangle(right1, right2, right3);
+Triangle sumTriangle = leftTriangle +  rightTriangle;
+Console.WriteLine("First triangle is EQUAL to the second triangle: " + (leftTriangle == rightTriangle));
+Console.WriteLine("First triangle is NOT equal to the second triangle: " + (leftTriangle != rightTriangle));
+Console.WriteLine("First triangle is GREATER than the second triangle: " + (leftTriangle > rightTriangle));
+Console.WriteLine("First triangle is NOT GREATER than the second triangle: " + (leftTriangle < rightTriangle));
+Triangle sameSide = (Triangle)5;
+Console.WriteLine("Side of equilateral triangle: " + sameSide.getPerimeter()/3);
---
+++ Day06/ConsoleApp1/ConsoleApp1/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Triangle
+    {
+        private double firstSide, secondSide, thirdSide;
+        public Triangle(double side1, double side2, double side3) {
+            if (!checkInequality(side1, side2, side3))
+            {
+                throw new ArgumentException("This triangle can not exist");
+            }
+            if (side1 <= 0||side2<=0||side3<=0)
+            {
+                throw new ArgumentOutOfRangeException("Input must be greater than 0");
+            }
+            
+            firstSide = side1;
+            secondSide = side2;
+            thirdSide = side3;
+        }
+        private bool checkInequality(double s1, double s2, double s3)
+        {
+            return (s1 + s2 >= s3);
+        }
+        public double getPerimeter() { 
+        return firstSide + secondSide + thirdSide;
+        }
+        public double getArea() {
+            double halfP = getPerimeter() / 2;
+            return Math.Sqrt(halfP*(halfP-firstSide)*(halfP-secondSide)*(halfP-thirdSide));
+        }
+        public static bool operator ==(Triangle left, Triangle right)
+        {
+            return left.firstSide == right.firstSide && left.secondSide == right.secondSide && left.thirdSide == right.thirdSide;
+        }
+        public static bool operator !=(Triangle left, Triangle right)
+        {
+            return left.firstSide != right.firstSide || left.secondSide != right.secondSide || left.thirdSide != right.thirdSide;
+        }
+        public static bool operator >(Triangle left, Triangle right) { 
+            return left.getArea() > right.getArea();
+        }
+        public static bool operator <(Triangle left, Triangle right)
+        {
+            return left.getArea() < right.getArea();
+        }
+        public static Triangle operator +(Triangle left, Triangle right)
+        {
+            double sumTriangle = left.getArea()+right.getArea();
+            double rightSide = Math.Sqrt(sumTriangle);
+            return new Triangle(left.firstSide, left.secondSide, rightSide);
+        }
+        public static explicit operator Triangle(double side)
+        {
+
+            return new Triangle(side, side, side);
+        }
+    }
+}
